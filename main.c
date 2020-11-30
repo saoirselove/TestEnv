acd4e7692b721a785f41bc6e0600908d9c465b83
saoirselove <1105187527@qq.com>
2020-11-30 13:40:51 +0800

    advise main

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-
+typedef struct TableRecord T;
+struct TableRecord{
+    int vertex;
+    int dist;
+};
 int main()
 {
+    T Tacle[10];
     printf("Hello world\n");
     return 0;
 }
